0478e74117ba70a0ee8ccb75a80cac9455528439
Farhan AP <Farhanadika7@gmail.com>
2024-01-09 19:45:42 +0700

    bunch of pages added

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Comic_Neue, Lusitana } from 'next/font/google'
+
+export const inter = Inter({subsets: ['latin']})
+export const comicNeue = Comic_Neue({subsets: ['latin'], weight: ['300']})
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400']})
